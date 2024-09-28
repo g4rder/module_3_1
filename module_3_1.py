e389a54ca93b293d391fa9ecea4fa02706fe7231
g4rder <ilyagarder@mail.ru>
2024-09-28 10:08:27 +0700

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,29 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    print((len(string), string.upper(), string.lower()))
+    count_calls()
+    return string
+
+
+def is_contains(string, is_list):
+    _flag = True
+    for i in is_list:
+        if i.lower() == string.lower():
+            _flag = True
+        else:
+            _flag = False
+    count_calls()
+    print(_flag)
+    return
+
+
+string_info('Capybara')
+string_info('Armageddon')
+is_contains('Urban', ['ban', 'BaNaN', 'urBAN']) # Urban ~ urBAN
+is_contains('cycle', ['recycling', 'cyclic']) # No matches
+print(calls)
